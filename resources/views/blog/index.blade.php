2b14546382ad4d52d0f8e4518e64e5fcc6128648
DenoBRL <denovan.brule@gmail.com>
2024-06-12 09:54:34 +0200

    commit

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -96,27 +96,129 @@
                     </div>
                 </div>
 
-                <footer class="px-6 py-2 text-gray-100 bg-gray-800">
-                    <div class="container flex flex-col items-center justify-between mx-auto md:flex-row"><a
-                            href="#" class="text-2xl font-bold">Brand</a>
-                        <p class="mt-2 md:mt-0">All rights reserved 2020.</p>
-                        <div class="flex mt-4 mb-2 -mx-2 md:mt-0 md:mb-0"><a href="#"
-                                class="mx-2 text-gray-100 hover:text-gray-400"><svg viewBox="0 0 512 512"
-                                    class="w-4 h-4 fill-current">
-                                    <path
-                                        d="M444.17,32H70.28C49.85,32,32,46.7,32,66.89V441.61C32,461.91,49.85,480,70.28,480H444.06C464.6,480,480,461.79,480,441.61V66.89C480.12,46.7,464.6,32,444.17,32ZM170.87,405.43H106.69V205.88h64.18ZM141,175.54h-.46c-20.54,0-33.84-15.29-33.84-34.43,0-19.49,13.65-34.42,34.65-34.42s33.85,14.82,34.31,34.42C175.65,160.25,162.35,175.54,141,175.54ZM405.43,405.43H341.25V296.32c0-26.14-9.34-44-32.56-44-17.74,0-28.24,12-32.91,23.69-1.75,4.2-2.22,9.92-2.22,15.76V405.43H209.38V205.88h64.18v27.77c9.34-13.3,23.93-32.44,57.88-32.44,42.13,0,74,27.77,74,87.64Z">
-                                    </path>
-                                </svg></a><a href="#" class="mx-2 text-gray-100 hover:text-gray-400"><svg
-                                    viewBox="0 0 512 512" class="w-4 h-4 fill-current">
-                                    <path
-                                        d="M455.27,32H56.73A24.74,24.74,0,0,0,32,56.73V455.27A24.74,24.74,0,0,0,56.73,480H256V304H202.45V240H256V189c0-57.86,40.13-89.36,91.82-89.36,24.73,0,51.33,1.86,57.51,2.68v60.43H364.15c-28.12,0-33.48,13.3-33.48,32.9V240h67l-8.75,64H330.67V480h124.6A24.74,24.74,0,0,0,480,455.27V56.73A24.74,24.74,0,0,0,455.27,32Z">
-                                    </path>
-                                </svg></a><a href="#" class="mx-2 text-gray-100 hover:text-gray-400"><svg
-                                    viewBox="0 0 512 512" class="w-4 h-4 fill-current">
-                                    <path
-                                        d="M496,109.5a201.8,201.8,0,0,1-56.55,15.3,97.51,97.51,0,0,0,43.33-53.6,197.74,197.74,0,0,1-62.56,23.5A99.14,99.14,0,0,0,348.31,64c-54.42,0-98.46,43.4-98.46,96.9a93.21,93.21,0,0,0,2.54,22.1,280.7,280.7,0,0,1-203-101.3A95.69,95.69,0,0,0,36,130.4C36,164,53.53,193.7,80,211.1A97.5,97.5,0,0,1,35.22,199v1.2c0,47,34,86.1,79,95a100.76,100.76,0,0,1-25.94,3.4,94.38,94.38,0,0,1-18.51-1.8c12.51,38.5,48.92,66.5,92.05,67.3A199.59,199.59,0,0,1,39.5,405.6,203,203,0,0,1,16,404.2,278.68,278.68,0,0,0,166.74,448c181.36,0,280.44-147.7,280.44-275.8,0-4.2-.11-8.4-.31-12.5A198.48,198.48,0,0,0,496,109.5Z">
-                                    </path>
-                                </svg></a>
+                <footer class="bg-gray-800 dark:bg-gray-900">
+                    <div class="mx-auto w-full max-w-screen-xl">
+                        <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
+                            <div>
+                                <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Entreprise
+                                </h2>
+                                <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                                    <li class="mb-4">
+                                        <a href="#" class=" hover:underline">À propos</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Emplois</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Centre de marque</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Blog</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Centre
+                                    d'aide</h2>
+                                <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Discord Server</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Twitter</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Facebook</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Légal</h2>
+                                <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Politique de confidentialité</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Licences</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Conditions générales</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Telecherger
+                                </h2>
+                                <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">iOS</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Android</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">Windows</a>
+                                    </li>
+                                    <li class="mb-4">
+                                        <a href="#" class="hover:underline">MacOS</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="px-4 py-6 bg-black dark:bg-gray-900 md:flex md:items-center md:justify-between">
+                            <span class="text-sm text-gray-400 dark:text-gray-300 sm:text-center">© 2024 <a
+                                    href="https://flowbite.com/">Laravel Vite Tailwind Flowbite™</a>. All Rights
+                                Reserved.
+                            </span>
+                            <div class="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
+                                <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+                                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor" viewBox="0 0 8 19">
+                                        <path fill-rule="evenodd"
+                                            d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="sr-only">Facebook page</span>
+                                </a>
+                                <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+                                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor" viewBox="0 0 21 16">
+                                        <path
+                                            d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
+                                    </svg>
+                                    <span class="sr-only">Discord community</span>
+                                </a>
+                                <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+                                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor" viewBox="0 0 20 17">
+                                        <path fill-rule="evenodd"
+                                            d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="sr-only">Twitter page</span>
+                                </a>
+                                <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+                                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                            d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="sr-only">GitHub account</span>
+                                </a>
+                                <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+                                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                            d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="sr-only">Dribbble account</span>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </footer>
